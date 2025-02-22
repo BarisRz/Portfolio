11fb69d66b02bf28d926fdba5689ab1a2e45f0fa
Baris G <barisg@pm.me>
2025-02-22 12:03:41 +0100

    Update .gitignore and Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -49,47 +49,10 @@ function Contact() {
       >
         Me <span className="text-secondary">contacter</span>
       </motion.p>
-      <form
-        ref={form}
-        onSubmit={sendEmail}
-        className="flex flex-col w-1/2 max-[500px]:w-11/12 max-[1440px]:w-3/4"
-      >
-        <label htmlFor="from_name" className="text-lg font-bold">
-          Name
-        </label>
-        <input
-          type="text"
-          name="from_name"
-          required
-          id="from_name"
-          className="p-2"
-        />
-        <label htmlFor="from_email" className="text-lg font-bold">
-          Email
-        </label>
-        <input
-          type="email"
-          name="from_email"
-          required
-          id="from_email"
-          className="p-2"
-        />
-        <label className="text-lg font-bold" htmlFor="sujet">
-          Sujet
-        </label>
-        <input type="text" name="sujet" id="sujet" className="p-2" />
-        <label htmlFor="message" className="text-lg font-bold">
-          Message
-        </label>
-        <textarea name="message" id="message" className="p-2" />
-        <button
-          type="submit"
-          value="Send"
-          className="mt-6 bg-primary hover:bg-secondary hover:text-white transition p-2 text-secondary font-bold"
-        >
-          Envoyer
-        </button>
-      </form>
+      <div className="flex flex-col items-center font-bold text-3xl">
+        <p>Mail : baris.gunay.pro@gmail.com</p>
+        <p>Téléphone : 0781135767</p>
+      </div>
     </>
   );
 }
